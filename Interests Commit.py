cab5f6d419413a5e7306d119d37f0f2a321760f6
KamiC0des <kamirae123@icloud.com>
2025-09-19 17:55:52 -0400

    Delete Interests Commit.py

--- Interests Commit.py
+++
@@ -1,5 +0,0 @@
-current_skills = {"Python basics", "Java Basics", "Problem solving", "Time management", "Data Structures"}
-skills_to_learn = {"Cooking", "Web design", "C++", "Git", "Public speaking"}
-career_interests = {"Software development", "Web development", "Cybersecurity", "Game development"}
-student_hobbies = {"Gaming", "Dancing", "Fitness", "Drawing", "Music"}
-entertainment_backlog = {"Netflix", "Disney+", "Horror Movies", "Prime Video"}
